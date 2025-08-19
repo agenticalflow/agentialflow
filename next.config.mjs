89bb849059dde96be1200d6d7d1a192a2b037af7
Gal Bracha <galbra@gmail.com>
2025-08-19 21:29:32 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/agentialflow',
-  assetPrefix: '/agentialflow/',
   eslint: {
     ignoreDuringBuilds: true,
   },
